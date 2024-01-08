3265eb7943e045590cda2e04b62260f7f657d94a
anass-gouzgar <go.anass.gouzgar@gmail.com>
2024-01-08 23:37:51 +0100

    managing-state-in-react TASK

---
+++ React Basics/WEEK2/managing_state_in_react/src/Fruits.js
@@ -0,0 +1,12 @@
+function Fruits(props) {
+    
+  return (
+    <div>
+      {props.fruits.map((f) => (
+        <p key={f.id}>{f.fruitName}</p>
+      ))}
+    </div>
+  );
+}
+
+export default Fruits;
