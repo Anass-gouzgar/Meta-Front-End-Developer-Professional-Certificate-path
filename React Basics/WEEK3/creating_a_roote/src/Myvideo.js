de044c302590e9e4473ab9884e9b15be29c47b99
anass-gouzgar <go.anass.gouzgar@gmail.com>
2024-01-09 22:30:46 +0100

    Build-a-calculator-app-in-react-js-coursera-assignment

--- React Basics/WEEK3/creating_a_roote/src/Myvideo.js
+++ React Basics/WEEK3/creating_a_roote/src/Myvideo.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactPlayer from "react-player/youtube";
 const Myvideo = () => {
+    const videoUrl="https://youtu.be/Pf03RTjeISE"
   return (
     <div className="video">
-      <ReactPlayer controls url="https://youtu.be/Pf03RTjeISE" />
+      <ReactPlayer controls url={videoUrl} />
     </div>
   );
 }
