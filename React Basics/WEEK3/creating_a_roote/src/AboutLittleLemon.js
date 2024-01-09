1d36395467b853c7afbaca34fe7cd685c26e23ab
anass-gouzgar <go.anass.gouzgar@gmail.com>
2024-01-09 19:30:00 +0100

    linking and routing TASK

---
+++ React Basics/WEEK3/creating_a_roote/src/AboutLittleLemon.js
@@ -0,0 +1,5 @@
+function AboutLittleLemon() {
+  return <div><h1>About Little Lemon</h1></div>;
+}
+
+export default AboutLittleLemon;
---
+++ React Basics/WEEK3/creating_a_roote/src/Homepage.js
@@ -0,0 +1,9 @@
+function Homepage() {
+  return (
+    <div>
+      <h1>Welcome to the Little Lemon site</h1>
+    </div>
+  );
+}
+
+export default Homepage;
